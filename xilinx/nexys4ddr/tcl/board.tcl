976ada03423f2368ceb5440cfe8b64e791a94152
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-06-17 19:04:22 +0900

    ボード定義ファイルを修正

--- xilinx/nexys4ddr/tcl/board.tcl
+++ xilinx/nexys4ddr/tcl/board.tcl
@@ -1,5 +1,6 @@
 # See LICENSE for license details.
-set name {arty}
-set part_fpga {xc7a35ticsg324-1L}
-set part_board {digilentinc.com:arty:part0:1.1}
+# See LICENSE.HORIE_Tetsuya for license details.
+set name {nexys4_ddr}
+set part_fpga {xc7a100tcsg324-1}
+set part_board {digilentinc.com:nexys4_ddr:part0:1.1}
 set bootrom_inst {rom}
